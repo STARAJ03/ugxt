4b71ab095dacb9ba0358a5d9e500f54306638f9d
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-09-01 15:28:59 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 import signal
 from pyrogram import idle
-from modules import ALL_MODULES
+from Extractor.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
 
